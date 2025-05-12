d07ee3ae894cea22e7bcd73b0a5282fd729cb1e2
J1741 <84889065+J1741@users.noreply.github.com>
2025-05-12 06:33:15 -0400

    modifying to add genderidentity question, correcting typos in some todos

--- personitems.js
+++ personitems.js
@@ -21,15 +21,15 @@ define(['questAPI'], function(Quest){
 		
 		questions: [
 
-		// Q4 TODO change to gender identity, keep dropd, and move down
+		// Q4 TODO gender identity, move down
 	    {
 	        inherit: 'dropd',
 	        name: 'eyecolor',
 	        stem: 'Please choose which eye-color you have:',
-	        answers: ['Blue-eyed', 'Green-eyed', 'Gray-eyed', 'Brown-eyed', 'Black-eyed', 'Hazel-Eyed']
+	        answers: ['Man', 'Woman', 'Non-binary OR genderqueer OR agender OR gender non-conforming OR another identity not listed here', 'Decline to answer']
 	    },
 
-		// TODO: Q2 birthmonth, keep as-is, and move down
+		// TODO: Q2 birthmonth, move down
 	    {
 	        inherit: 'dropd',
 	        name: 'birthyear',
@@ -37,7 +37,7 @@ define(['questAPI'], function(Quest){
 	        answers: [2004,2003,2002,2001,2000,1999,1998,1997,1996,1995,1994,1993,1992,1991,1990,1989,1988,1987,1986,1985,1984,1983,1982,1981,1980,1979,1978,1977,1976,1975,1974,1973,1972,1971,1970,1969,1968,1967,1966,1965,1964,1963,1962,1961,1960,1959,1958,1957,1956,1955,1954,1953,1952,1951,1950,1949,1948,1947,1946,1945,1944,1943,1942,1941,1940,1939,1938,1937,1936,1935,1934,1933,1932,1931,1930,1929,1928,1927,1926,1925,1924,1923,1922,1921,1920]
 	    },
 
-		// TODO: Q1 birthyear, move to top and keep as-is
+		// TODO: Q1 birthyear, move to top
 	    {
 	        inherit: 'dropd',
 	        name: 'birthmonth',
@@ -45,7 +45,7 @@ define(['questAPI'], function(Quest){
 	        answers: ["January", "February", "March", "April", "May", "June", "July","August","September","October", "November", "December"]
 	    },
 
-		// Q3 TODO add race and make selectMulti
+		// Q3 TODO race, add and make selectMulti
 
 		// TODO: remove question
 	    {
@@ -55,10 +55,10 @@ define(['questAPI'], function(Quest){
 	        answers: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 	    },
 
-		// Q5 TODO add zipcurrent and make text
+		// Q5 TODO zipcurrent, add and make text
 		// {},
 
-		// Q6 TODO add ziplongest and make text
+		// Q6 TODO ziplongest, add and make text
 		// {},
 
 		]
